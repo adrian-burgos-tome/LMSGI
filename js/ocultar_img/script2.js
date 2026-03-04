48cba6e77a6503150c3f723fa8f9d33fc77e5458
adrian-burgos-tome <aburtom0312@g.educaand.es>
2026-03-04 14:32:25 +0100

    carrusel con temporizador

---
+++ js/ocultar_img/script2.js
@@ -0,0 +1,19 @@
+const imagenes = document.querySelectorAll('.carrusel img');
+
+let diapositivaActual=0;
+
+mostrarCarrusel();
+
+
+function mostrarCarrusel(){
+    for (let i = 0; i < imagenes.length; i++) {
+                imagenes[i].style.display = 'none';
+            }
+            imagenes[diapositivaActual].style.display = 'block';
+    
+    diapositivaActual++;
+    if(diapositivaActual>=3){
+        diapositivaActual=0;
+    }
+    setTimeout (mostrarCarrusel,2000);
+}
