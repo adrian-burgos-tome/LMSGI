c53c00af3845f61c3e8f23f0b01a0eb92736ec47
adrian-burgos-tome <aburtom0312@g.educaand.es>
2026-03-18 13:08:03 +0100

    Ejercicio borrar dinamicamente

--- ejercicios_js/ejercicio5/script.js
+++ ejercicios_js/ejercicio5/script.js
@@ -1,14 +1,40 @@
 const botones = document.querySelectorAll('.borrar');
+const listatareas = document.getElementById('lista');
+const entrada = document.getElementById('entrada');
+const button = document.getElementById('agregar');
 
-botones.forEach(boton => {
+button.addEventListener('click', agregar);
+
+
+
+
+function agregar() {
+    //Si el input esta facio, o lleno con solamente espacios, este if transformara "     " en ""
+    //De manera que con return, para el programa
+    if (entrada.value.trim() === "") return; 
+
+    const litarea = document.createElement("li");
+    const node = document.createTextNode(entrada.value + " ");
+    litarea.appendChild(node);
+
     
-    boton.addEventListener('click', borrarElemento);
+    const nuevoBoton = document.createElement("button");
+    nuevoBoton.innerText = "X";
+    nuevoBoton.classList.add("borrar");
 
-});
+    
+    nuevoBoton.addEventListener('click', borrarElemento);
+
+    
+    litarea.appendChild(nuevoBoton);
+    listatareas.appendChild(litarea);
+    //borrar contenido automaticamente
+    entrada.value = ""; 
+}
 
 function borrarElemento() {
-    
+
     const li = this.parentElement; 
     li.remove();
-
+    
 }
